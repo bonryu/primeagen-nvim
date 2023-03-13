d8a355f9447378cb3b12519adcb7883773fc38a7
Bon Ryu <bonryu@gmail.com>
2023-03-13 03:42:44 -0400

    modified set.lua

--- lua/bonryu/set.lua
+++ lua/bonryu/set.lua
@@ -1,36 +1,59 @@
+-- :help options
+-- :set
 vim.g.mapleader = " "
--- vim.opt.guicursor = ""
 
-vim.opt.nu = true
-vim.opt.relativenumber = true
+local options = {
+    -- guicursor = "",                      -- keep rectangle cursor in insert mode
+    cmdheight = 2,                          -- more space in the neovim command line for displaying messages
+    completeopt = { "menuone", "noselect"}, -- mostly just for cmp
+    conceallevel = 0,                       -- so that `` is visible in markdonw files
+    fileencoding = "utf-8",                 -- the encoding writtent to a file
+    nu = true,                              -- number, nu, nonumber, nonu. Line numbering
+    pumheight = 10,                         -- pop up menu height
+    showmode = true,                        -- keep showing the mode
+    -- showtabline = 2,                        -- always show tabs
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
+    ignorecase = true,                      -- ignore case in search patterns
+    smartcase = true,                       -- smart case
+    smartindent = true,
+    breakindent = true,
+    breakindentopt = { 'shift:2' },
+    wrap = true,
+    linebreak = true,
 
-vim.opt.smartindent = true
-vim.opt.breakindent = true
-vim.opt.breakindentopt = { 'shift:2' }
+    -- Splits
+    splitbelow = true,                      -- force all horizontal splits to go below current window
+    splitright = true,                      -- force all vertical splits to go to the right of the current window
 
-vim.opt.wrap = true
-vim.opt.linebreak = true
+    -- Undo tree, swap, backup related, etc.
+    swapfile = false,
+    undodir = os.getenv("HOME") .. "/.vim/undodir",
+    undofile = true,                        -- enable persistent undo
+    -- writebackup = false,                    -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
 
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-vim.opt.undofile = true
+    cursorline = true,                     -- highlight the current line
+    relativenumber = true,
+    tabstop = 4,
+    softtabstop = 4,
+    shiftwidth = 4,
+    expandtab = true,
+    backup = false,
+    hlsearch = false,                       -- don't highlight all matches of previous search
+    incsearch = true,
 
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
 
-vim.opt.termguicolors = true
+    termguicolors = true,
+    scrolloff = 8,                          -- minimal number of screen lines to keep above and below the cursor
+    sidescrolloff = 8,                      -- minimal number of screen columns either side of cursor if wrap is `false`
+    signcolumn = "yes",                     -- always show the sign column, otherwise it would shift the text each time
+    updatetime = 50,
+    colorcolumn = "80",
+    guifont = "monospace:h17",              -- the font used in graphical neovim applications
+}
 
-vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
-vim.opt.isfname:append("@-@")
+for k, v in pairs(options) do
+    vim.opt[k] = v
+end
 
-vim.opt.updatetime = 50
-
-vim.opt.colorcolumn = "80"
-
+vim.opt.iskeyword:append "-"                -- hyphnated words recognized by searches as one word. e.g. asdf-asdf can be searched as asdfasdf
+vim.opt.isfname:append "@-@"
