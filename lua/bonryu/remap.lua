02a6da2f2195f34709aa2ba106c2ae373394007d
Bon Ryu <bonryu@gmail.com>
2023-03-18 06:16:21 -0400

    asdf

--- lua/bonryu/remap.lua
+++ lua/bonryu/remap.lua
@@ -1,4 +1,24 @@
+local opts = { noremap = true, silent = true }
+
+local term_opts = { silent = true}
+
+-- Shorten function name
+local keymap = vim.api.nvim_set_keymap
+
+-- Remap space as leader key
+keymap("", "<Space", "<Nop>", opts)
 vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Modes
+--  normal_mode = "n"
+--  insert_mode = "i"
+--  visula_mode = "v",
+--  visual_block_mode = "x",
+--  term_mode = "t",
+--  command_mode = "c",
+
+
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- move lines up and down in visual mode
@@ -56,7 +76,8 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
-vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>");
+-- vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>");
+vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.config/nvim/lua/bonryu/packer.lua<CR>");
 vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
 
 vim.keymap.set("n", "<leader><leader>", function()
