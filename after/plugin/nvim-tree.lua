4976c49fc2f3d4c211610557a3a8c8ab4dfede51
Bon Ryu <bonryu@gmail.com>
2023-04-29 18:43:14 -0400

    update to nvim-tree.lua to accomodate upgrade to how on_attach works

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -109,27 +109,27 @@ nvim_tree.setup {
 }
 
 
-local function open_nvim_tree(data)
-    -- buffer is a directory
-    local directory = vim.fn.isdirectory(data.file) == 1
-
-    if not directory then
-        return
-    end
-
-    -- Uncomment below lines to open a new buffer by default besides nvimtree
-    -- create a new, empty buffer
-    vim.cmd.enew()
-
-    -- wipe the directory buffer
-    vim.cmd.bw(data.buf)
-
-    -- change to the directory
-    vim.cmd.cd(data.file)
-
-    -- open the tree
-    require("nvim-tree.api").tree.open()
-end
-
-vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+-- local function open_nvim_tree(data)
+--     -- buffer is a directory
+--     local directory = vim.fn.isdirectory(data.file) == 1
+-- 
+--     if not directory then
+--         return
+--     end
+-- 
+--     -- Uncomment below lines to open a new buffer by default besides nvimtree
+--     -- create a new, empty buffer
+--     vim.cmd.enew()
+-- 
+--     -- wipe the directory buffer
+--     vim.cmd.bw(data.buf)
+-- 
+--     -- change to the directory
+--     vim.cmd.cd(data.file)
+-- 
+--     -- open the tree
+--     require("nvim-tree.api").tree.open()
+-- end
+-- 
+-- vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
 
